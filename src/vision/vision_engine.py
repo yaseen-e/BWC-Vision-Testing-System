f8c4a1f9bdccbb7ed9f559591134099aba9a8b21
Yaseen Elaraby <143433702+yaseen-e@users.noreply.github.com>
2026-06-04 13:50:46 -0400

    size normal again 1280x720

--- src/vision/vision_engine.py
+++ src/vision/vision_engine.py
@@ -438,7 +438,7 @@ def _get_camera() -> Optional[Any]:
 		# Use the first available camera
 		camera_num = camera_info[0]["Num"]
 		camera = Picamera2(camera_num=camera_num)
-		config = camera.create_preview_configuration(main={"size": (640, 480)})
+		config = camera.create_preview_configuration(main={"size": (1280, 720)})
 		camera.configure(config)
 		camera.start()
 		camera.set_controls({"AfMode": 2})
