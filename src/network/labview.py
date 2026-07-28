073f2a57f9f36670bfe2857a90ae712fba8d6c75
Yaseen Elaraby <143433702+yaseen-e@users.noreply.github.com>
2026-07-28 04:07:47 -0400

    final sanity check

--- src/network/labview.py
+++ src/network/labview.py
@@ -55,18 +55,18 @@ _SIMULATED_COMMANDS: collections.deque[str] = collections.deque(
 		# C1-C7: HS
 		#"BACK", "RUN_OCR", "BACK", "RUN_OCR", "BACK", "RUN_OCR", "BACK", "RUN_OCR", "BACK", "RUN_OCR", "BACK", "RUN_OCR", "BACK", "RUN_OCR",
 		# C8: SST
-		# "MENU", "DOWN", "SELECT", "RUN_OCR",
+		"MENU", "DOWN", "SELECT", "RUN_OCR",
 		# C9: SSB
-		# "DOWN", "RUN_OCR",
+		"DOWN", "RUN_OCR",
 		# "UP", "BACK", "BACK",
 		# C10-C13: LOC
-		# "MENU", "RIGHT", "DOWN", "SELECT", "DOWN", "DOWN", "SELECT", "DOWN", "DOWN", "SELECT",
+		"MENU", "RIGHT", "DOWN", "SELECT", "DOWN", "DOWN", "SELECT", "DOWN", "DOWN", "SELECT",
 		# USA
-		# "SELECT", "SELECT", "RUN_OCR",
+		"SELECT", "SELECT", "RUN_OCR",
 		# CANADA
 		# "LEFT", "SELECT", "DOWN", "SELECT", "RUN_OCR",
 		# MEXICO
-		# "LEFT", "SELECT", "DOWN", "DOWN", "SELECT", "RUN_OCR",
+		"LEFT", "SELECT", "DOWN", "DOWN", "SELECT", "RUN_OCR",
 		# OTHER
 		# "LEFT", "SELECT", "DOWN", "DOWN", "DOWN", "SELECT", "RUN_OCR",
 		# "BACK", "BACK", "BACK", "BACK",
